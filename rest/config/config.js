09142f1848f172a12d9f673f7bfa0da73f8d47bd
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-20 08:15:38 +0700

    Update CSS

--- rest/config/config.js
+++ rest/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 9999,
-        dbURL: 'mongodb://localhost:27017/rest-api-db',
+        dbURL: 'mongodb://localhost:27017/tcd0302-rest-api-db',
         authCookieName: 'x-auth-token'
     },
     production: {}
